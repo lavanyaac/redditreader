abf6d645c00108d12674bf757cb40efa9e5099b0
Lavanya Ammi Chandrashekara <lavanya.ammic@gmail.com>
2017-11-29 14:28:26 -0800

    Add minimal css

--- src/Listings/Listing.js
+++ src/Listings/Listing.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 
 class Listing extends Component {
 	render() {
-		const {listing, index} = this.props;
+		const {listing} = this.props;
+    const defaultImageSrc = "assets/images/thumbnail-default.jpg";
+    const imgSrc = ['default', 'self', 'nsfw'].includes(listing.data.thumbnail)? 
+    defaultImageSrc : listing.data.thumbnail;
     return (
       <li className="listing-container">
-        <p>{listing.data.title}</p>
-        <p>{listing.data.domain}</p>
-        <p>{listing.data.author}</p>
-        <p>{listing.data.subreddit_name_prefixed}</p>
-        <p>{listing.data.num_comments}</p>
+        <div><p>{listing.data.ups}</p></div>
+        <div className="listing-image"><img src={imgSrc} alt="" /></div>
+        <div>
+          <p>{listing.data.title}</p>
+          <p>{listing.data.domain}</p>
+          <p>{listing.data.author}</p>
+          <p>{listing.data.subreddit_name_prefixed}</p>
+          <p>{listing.data.num_comments}</p>
+        </div>
       </li>
 
     );
